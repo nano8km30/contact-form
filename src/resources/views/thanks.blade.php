497993fe05d106790a89ccf2e4ee8f68af55a346
sae <nano.8km.30@gmail.com>
2025-12-02 19:04:19 +0900

    まとめてコミット

---
+++ src/resources/views/thanks.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <a class="header__logo" href="/">
+                Contact Form
+            </a>
+        </div>
+    </header>
+
+    <main>
+        <div class="thanks__content">
+            <div class="yhanks__heading">
+                <h2>お問い合わせありがとうございます</h2>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
